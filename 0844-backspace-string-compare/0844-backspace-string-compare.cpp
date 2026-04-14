34b5b89ab82b31499b5f3a83d80d1d5ab412f77e
ShivaGanesh.V <vsgvarikuppala@gmail.com>
2026-04-14 18:31:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.8%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+    string process(string str){
+        string result;
+
+        for(char ch : str){
+            if(ch == '#'){
+                if(!result.empty()){
+                    result.pop_back();
+                }
+            } else {
+                result.push_back(ch);
+            }
+        }
+        return result;
+    }
+    bool backspaceCompare(string s, string t) {
+        return process(s) == process(t);
+    }
+};
